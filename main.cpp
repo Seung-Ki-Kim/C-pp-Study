06309d84c671de83dbb85b3f4873a67afb049334
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-10-07 16:53:28 +0900

    2023.10.07

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto dnum = 2.3;
+    auto dnum_ = {2.34};
+
+    cout << dnum;	// Result: 2
+
     return 0;
 }
