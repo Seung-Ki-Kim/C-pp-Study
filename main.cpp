1aef28e1ddad7aca5d1072393adeb7fc2493bee1
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-11-04 19:57:19 +0900

    2023.11.04

--- main.cpp
+++ main.cpp
@@ -1,35 +1,64 @@
 #include <iostream>
 using namespace std;
 
+enum class Color {
+    COLOR_BLACK,
+    COLOR_WHITE,
+    COLOR_RED,
+    COLOR_BLUE,
+    COLOR_GREEN,
+};
+
+enum class Traffic_light {
+    TRAFFIC_RED,
+    TRAFFIC_GREEN,
+    TRAFFIC_YELLOW
+};
+
+Traffic_light& operator++(Traffic_light& t) {
+    switch(t) {
+        case Traffic_light::TRAFFIC_GREEN :
+            return t = Traffic_light::TRAFFIC_YELLOW;
+        case Traffic_light::TRAFFIC_RED :
+            return t = Traffic_light::TRAFFIC_GREEN;
+        case Traffic_light::TRAFFIC_YELLOW :
+            return t = Traffic_light::TRAFFIC_RED;
+    }
+}
+
 class Circle {
 private:
     double radius;
-    string color;
+    Color color;
 
 public:
-    Circle(double radius, string color) {
+    Circle(double radius, Color color) {
         this->radius = radius;
         this->color = color;
     }
 
     double calArea();
-    string colorPrint();
+    Color getColor();
 };
 
 double Circle::calArea() {
     return 3.14 * radius * radius;
 }
 
-string Circle::colorPrint() {
+Color Circle::getColor() {
     return color;
 }
 
 
 int main() {
-    Circle circle(5, "Black");
+    Circle circle(5, Color::COLOR_BLACK);
+    Traffic_light next;
+
+
 
     cout << circle.calArea() << '\n';
-    cout << circle.colorPrint() << '\n';
+    cout << static_cast<int>(circle.getColor()) << '\n';
+    cout << static_cast<int>(++next) << '\n';
 
     return 0;
 }
