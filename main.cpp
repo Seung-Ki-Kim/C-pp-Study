eaa775bf3ac2e357f5d947ca88f7a6df72aade65
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-11-05 19:41:13 +0900

    2023.11.05

--- main.cpp
+++ main.cpp
@@ -1,64 +1,35 @@
 #include <iostream>
 using namespace std;
 
-enum class Color {
-    COLOR_BLACK,
-    COLOR_WHITE,
-    COLOR_RED,
-    COLOR_BLUE,
-    COLOR_GREEN,
+class Vector {
+public :
+    Vector(int s);
+    double& operator[](int i);
+    int size();
+private :
+    double* elem;
+    int sz;
 };
 
-enum class Traffic_light {
-    TRAFFIC_RED,
-    TRAFFIC_GREEN,
-    TRAFFIC_YELLOW
-};
-
-Traffic_light& operator++(Traffic_light& t) {
-    switch(t) {
-        case Traffic_light::TRAFFIC_GREEN :
-            return t = Traffic_light::TRAFFIC_YELLOW;
-        case Traffic_light::TRAFFIC_RED :
-            return t = Traffic_light::TRAFFIC_GREEN;
-        case Traffic_light::TRAFFIC_YELLOW :
-            return t = Traffic_light::TRAFFIC_RED;
-    }
+Vector::Vector(int s):elem{new double[s]},sz{s} { // Constructor
+    // Vector algorithm
 }
 
-class Circle {
-private:
-    double radius;
-    Color color;
-
-public:
-    Circle(double radius, Color color) {
-        this->radius = radius;
-        this->color = color;
-    }
-
-    double calArea();
-    Color getColor();
-};
-
-double Circle::calArea() {
-    return 3.14 * radius * radius;
+double& Vector::operator[](int i) {
+    return elem[i];
 }
 
-Color Circle::getColor() {
-    return color;
+int Vector::size() {
+    return sz;
 }
 
+double sqrt(double);    // Root function
+
+double sqrt(double d) { // Definition
+    // sqrt algorithm
+}
 
 int main() {
-    Circle circle(5, Color::COLOR_BLACK);
-    Traffic_light next;
-
-
-
-    cout << circle.calArea() << '\n';
-    cout << static_cast<int>(circle.getColor()) << '\n';
-    cout << static_cast<int>(++next) << '\n';
 
     return 0;
 }
