6706bdfc4ce89aec16ed208871eaa58235762e5b
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-11-10 14:46:49 +0900

    2023.11.10

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+namespace My_Code {
+    class complex {
+
+    };
+    complex sqrt(complex);
+    int main();
+}
+
+int My_Code::main() {
+    complex z {1, 2};
+    auto z2 = sqrt(z);
+    std::cout << '{' << z2.real() << ',' << z2.imag() << "}\n";
+}
+
+
 int main() {
 
-    return 0;
+    return My_Code::main();
 }
