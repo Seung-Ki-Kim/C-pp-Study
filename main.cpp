60f66e50e8b16231fa2525db8b5aab32b16a9f09
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-11-17 15:29:21 +0900

    2023.11.17

--- main.cpp
+++ main.cpp
@@ -1,22 +1,58 @@
 #include <iostream>
 using namespace std;
 
-namespace My_Code {
-    class complex {
+class complex {
+    double re, im;
 
-    };
-    complex sqrt(complex);
-    int main();
-}
+public:
+    complex(double r, double i) : re{r}, im{i} {
+        // Constructor
+    }
 
-int My_Code::main() {
-    complex z {1, 2};
-    auto z2 = sqrt(z);
-    std::cout << '{' << z2.real() << ',' << z2.imag() << "}\n";
-}
+    complex(double r) : re{r}, im{0} {
+        // Constructor
+    }
+
+    complex() : re{0}, im{0} {
+        // Constructor
+    }
+
+    double real() const {
+        return re;
+    }
+
+    void real(double d) {
+        re = d;
+    }
+
+    double image() const {
+        return im;
+    }
+
+    void imag(double d) {
+        im = d;
+    }
+
+    complex& operator+=(complex z) {
+        re += z.re;
+        im += z.im;
+
+        return *this;
+    }
+
+    complex& operator-=(complex z) {
+        re -= z.re;
+        im -= z.im;
+
+        return *this;
+    }
+
+    complex& operator*=(complex);
+    complex& operator/=(complex);
+};
 
 
 int main() {
 
-    return My_Code::main();
+    return 0;
 }
