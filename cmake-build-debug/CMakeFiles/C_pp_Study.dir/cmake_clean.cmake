3df685a925765414d4d9200b483899f9b89659bf
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-12-11 20:00:24 +0900

    2023.12.11

---
+++ cmake-build-debug/CMakeFiles/C_pp_Study.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_pp_Study.dir/Cat.cpp.o"
+  "CMakeFiles/C_pp_Study.dir/Cat.cpp.o.d"
+  "CMakeFiles/C_pp_Study.dir/Data.cpp.o"
+  "CMakeFiles/C_pp_Study.dir/Data.cpp.o.d"
+  "CMakeFiles/C_pp_Study.dir/Dog.cpp.o"
+  "CMakeFiles/C_pp_Study.dir/Dog.cpp.o.d"
+  "CMakeFiles/C_pp_Study.dir/main.cpp.o"
+  "CMakeFiles/C_pp_Study.dir/main.cpp.o.d"
+  "C_pp_Study"
+  "C_pp_Study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_pp_Study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,113 +1,14 @@
 #include <iostream>
-#include "Dog.h"
-#include "Cat.h"
-
+#include "Data.h"
 using namespace std;
-
-<<<<<<< Updated upstream
-class complex {
-    double re, im;
-
-public:
-    complex(double r, double i) : re{r}, im{i} {
-        // Constructor
-    }
-
-    complex(double r) : re{r}, im{0} {
-        // Constructor
-    }
-
-    complex() : re{0}, im{0} {
-        // Constructor
-    }
-
-    double real() const {
-        return re;
-    }
-
-    void real(double d) {
-        re = d;
-    }
-
-    double image() const {
-        return im;
-    }
-
-    void imag(double d) {
-        im = d;
-    }
-
-    complex& operator+=(complex z) {
-        re += z.re;
-        im += z.im;
-
-        return *this;
-    }
-
-    complex& operator-=(complex z) {
-        re -= z.re;
-        im -= z.im;
-
-        return *this;
-    }
-
-    complex& operator*=(complex);
-    complex& operator/=(complex);
-};
-=======
-
-template <typename T>
-void Swap(T& a, T& b) {
-    T temp;
-
-    temp = a;
-
-    a = b;
-    b = temp;
-}
-
-template <> void Swap<double>(double& a, double& b) {
-    // Pass
-}
->>>>>>> Stashed changes
 
 
 int main() {
-    int x = 2;
-    int y = 3;
+    Data<String_t> str_data("Hello, world!");
+    Data<Double_t> double_data(3.14);
 
-<<<<<<< Updated upstream
-=======
-    cout << "- Before" << endl;
-    cout << "x: " << x << ", y: " << y << endl;
+    cout << str_data.GetData() << endl;
+    cout << double_data.GetData() << endl;
 
-    Swap(x, y);
-
-    cout << "- After" << endl;
-    cout << "x: " << x << ", y: " << y << endl;
-
-    char i = 'i';
-    char j = 'j';
-
-    cout << "- Before" << endl;
-    cout << "i: " << i << ", j: " << j << endl;
-
-    Swap(i, j);
-
-    cout << "- After" << endl;
-    cout << "i: " << i << ", j: " << j << endl;
-
-    double l = 2.5;
-    double m = 3.5;
-
-    cout << "- Before" << endl;
-    cout << "l: " << l << ", m: " << m << endl;
-
-    Swap(l, m);
-
-    cout << "- After" << endl;
-    cout << "l: " << l << ", m: " << m << endl;
-
->>>>>>> Stashed changes
     return 0;
 }
