328cab6f945601b1044baecd8982a5df1413522e
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-12-27 15:41:52 +0900

    2023.12.27

--- main.cpp
+++ main.cpp
@@ -1,57 +1,29 @@
 #include <iostream>
-#include <mmcobj.h>
 
 using namespace std;
 
-enum Type {
-    ptr,
-    num
-};
-
 class Vector {
-public:
-    Vector(int s) : elem{new double[s]}, sz{s} {}
-    double& operator[](int i) { return elem[i]; }
-    int size() { return sz; }
 private:
     double* elem;
     int sz;
-};
-
-
-double read_and_sum(int s) {
-    Vector v(s);
-    double sum = 0;
-
-    for (int i = 0; i != v.size(); ++i) {
-        cin >> v[i];
-        sum += v[i];
+public:
+    Vector(int s) : elem{new double[s]}, sz{s} {
+        for (int i = 0; i != s; ++i) {
+            elem[i] = 0;
+        }
     }
 
-    return sum;
-}
-
-union Value {
-    Node *p;
-    int i;
-};
-
-struct Entry {
-    string name;
-    Type t;
-    Value v;
-};
-
-void f(Entry* pe) {
-    if (pe -> t == num) {
-        cout << pe -> v.i;
+    ~Vector() {
+        delete[] elem;
     }
-}
+
+    double& operator[](int i);
+    int size() const;
+};
 
 
 
 int main() {
-    cout << "Hello, world!" << "\n";
 
     return 0;
 }
