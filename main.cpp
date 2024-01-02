48888430f72dcb6b41bf55ba71b4dff9425d2b34
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2024-01-02 17:47:46 +0900

    2024.01.02

--- main.cpp
+++ main.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
-
 using namespace std;
 
 class Vector {
-private:
-    double* elem;
-    int sz;
 public:
-    Vector(int s) : elem{new double[s]}, sz{s} {
-        for (int i = 0; i != s; ++i) {
-            elem[i] = 0;
-        }
-    }
+    Vector();
+    Vector(std::initializer_list<double>);
+    void push_back(double);
 
-    ~Vector() {
-        delete[] elem;
-    }
-
-    double& operator[](int i);
-    int size() const;
+    double *elem;
+    int sz;
 };
 
+Vector read(istream& is) {
+    Vector v = {1, 2};
+
+    for (double d; is >> d;) {
+        v.push_back(d);
+    }
+
+    return v;
+}
+
+Vector::Vector(std::initializer_list<double> lst) : elem{new double[lst.size()]}, sz{static_cast<int>(lst.size())} {
+    copy(lst.begin(), lst.end(), elem);
+}
+
+
+class Container {
+public:
+    virtual double& operator[](int) = 0;
+    virtual int size() const = 0;
+    virtual ~Container() {}
+};
+
+void use(Container& c) {
+    const int sz = c.size();
+
+    for (int i = 0; i != sz; ++i) {
+        cout << c[i] << '\n';
+    }
+}
 
 
 int main() {
-
     return 0;
 }
