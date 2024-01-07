74e8e0eb88fb44bcda89f94c4740af79fcc5eb77
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2024-01-07 17:07:38 +0900

    2024.01.07

--- main.cpp
+++ main.cpp
@@ -1,45 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Vector {
+class Shape {
 public:
-    Vector();
-    Vector(std::initializer_list<double>);
-    void push_back(double);
+    virtual Point center() const = 0;
+    virtual void move(Point to) = 0;
+    virtual void draw() const = 0;
+    virtual void rotate(int angle) = 0;
 
-    double *elem;
-    int sz;
+    virtual ~Shape() {}
 };
 
-Vector read(istream& is) {
-    Vector v = {1, 2};
-
-    for (double d; is >> d;) {
-        v.push_back(d);
-    }
-
-    return v;
-}
-
-Vector::Vector(std::initializer_list<double> lst) : elem{new double[lst.size()]}, sz{static_cast<int>(lst.size())} {
-    copy(lst.begin(), lst.end(), elem);
-}
 
 
-class Container {
-public:
-    virtual double& operator[](int) = 0;
-    virtual int size() const = 0;
-    virtual ~Container() {}
-};
-
-void use(Container& c) {
-    const int sz = c.size();
-
-    for (int i = 0; i != sz; ++i) {
-        cout << c[i] << '\n';
-    }
-}
 
 
 int main() {
