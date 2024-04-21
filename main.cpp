a3d1931cdd424e2b1e6c820f950f64e0e57d1809
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2024-04-21 22:10:13 +0900

    2024.04.21

--- main.cpp
+++ main.cpp
@@ -15,25 +15,37 @@ struct perks {
         "Packard"
 };
 
+union one4all {
+    int int_val;
+    long long_val;
+    double double_val;
+};
 
 int main () {
-    inflatable guest = {
-            "Glorious Gloria",
-            1.88,
-            29.99
+    inflatable guests[2] = {
+            {"Bambi", 0.5, 21.99},
+            {"Godzilla", 2000, 565.99}
     };
     
-    inflatable pal = guest;
+    cout << guests[0].name << endl; 
+    cout << guests[1].name << endl;
     
-    struct {
-        int x;
-        int y;
-    } position;
+    one4all pail;
     
-    cout << "Expand your guest list with " << guest.name;
-    cout << " and " << pal.name << "!\n";
-    cout << "You can have both for $";
-    cout << guest.price + pal.price << "!\n";
+    pail.int_val = 15;
+    cout << pail.int_val << endl;
+    
+    pail.double_val = 1.38;
+    cout << pail.double_val << endl;
+    
+    cout << pail.int_val << endl;
+
+
+    enum bits{one = 1, two = 2, four = 4, eight = 8};
+    bits myflag;
+    
+    myflag = bits(9);
+    cout << myflag << endl;
     
     return 0;
 }
