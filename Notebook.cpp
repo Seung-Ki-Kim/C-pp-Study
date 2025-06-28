1c86c408b09adbf262f081dbb15b13e1a69c9f54
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2025-06-28 13:26:48 +0900

    챕터 3 연습 노트 갱신

--- Notebook.cpp
+++ Notebook.cpp
@@ -1,34 +1,42 @@
 #include <cstdio>
 
-struct College {
-	char name[256];
+struct Element {
+	Element* next{};
+	char prefix[2];
+	short operating_number;
+
+	void insert_after(Element* new_element) {
+		new_element -> next = this->next;
+		this->next = new_element;
+	}
 };
 
-void print_name(College* colleges, size_t n_colleges) {
-	for (size_t i = 0; i < n_colleges; i++) {
-		printf("%s College\n", colleges[i].name);
-	}
-}
-
 int main() {
-	char lower[] = "abc?e";
-	char upper[] = "ABC?E";
+	Element trooper1, trooper2, trooper3;
 
-	char* upper_ptr = upper;	// &upper[0]
+	trooper1.prefix[0] = 'T';
+	trooper1.prefix[1] = 'K';
+
+	trooper1.operating_number = 421;
+	trooper1.insert_after(&trooper2);
+
+	trooper2.prefix[0] = 'F';
+	trooper2.prefix[1] = 'N';
+
+	trooper2.operating_number = 2187;
+	trooper2.insert_after(&trooper3);
+
+	trooper3.prefix[0] = 'L';
+	trooper3.prefix[1] = 'S';
 	
-	lower[3] = 'd';
-	upper_ptr[3] = 'D';
+	trooper3.operating_number = 005;
 
-	char letter_d = lower[3];
-	char letter_D = upper_ptr[3];
-
-	printf("lower: %s\n", lower);
-	printf("upper: %s\n", upper);
-	
-	
-
-
-
+	for (Element* cursor = &trooper1; cursor != nullptr; cursor = cursor->next) {
+		printf("stormtrooper %c%c-%d\n", 
+				cursor->prefix[0],
+				cursor->prefix[1],
+				cursor->operating_number);
+	}
 
 
 	return 0;
