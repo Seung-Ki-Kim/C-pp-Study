1be8f3c5fe719d976aa256bb10d7baf1fe23deb9
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2025-06-28 19:36:58 +0900

    챕터 3 공부 완료

--- Notebook.cpp
+++ Notebook.cpp
@@ -1,43 +1,40 @@
 #include <cstdio>
 
-struct Element {
-	Element* next{};
-	char prefix[2];
-	short operating_number;
+struct ClockOfTheLongNow {
+	int year = 2019;
 
-	void insert_after(Element* new_element) {
-		new_element -> next = this->next;
-		this->next = new_element;
+	int get_year() const {
+		return year;
+	}
+	
+	bool is_leap_year(const ClockOfTheLongNow& clock) {
+		if (clock.get_year() % 4 > 0) {
+			return false;
+		}
+
+		return true;
+	}
+};
+
+struct Avout {
+	ClockOfTheLongNow apert;
+	const char* name;
+	
+
+	Avout(const char* name, long year_of_apert) : 
+		name{ name }, apert{ year_of_apert } {}
+
+	void announce() const {
+		printf("%s %d\n", name, apert.get_year());
 	}
 };
 
 int main() {
-	Element trooper1, trooper2, trooper3;
+	Avout raz { "Erasmas", 3010 };
+	Avout jad { "Jad", 4090 };
 
-	trooper1.prefix[0] = 'T';
-	trooper1.prefix[1] = 'K';
-
-	trooper1.operating_number = 421;
-	trooper1.insert_after(&trooper2);
-
-	trooper2.prefix[0] = 'F';
-	trooper2.prefix[1] = 'N';
-
-	trooper2.operating_number = 2187;
-	trooper2.insert_after(&trooper3);
-
-	trooper3.prefix[0] = 'L';
-	trooper3.prefix[1] = 'S';
-	
-	trooper3.operating_number = 005;
-
-	for (Element* cursor = &trooper1; cursor != nullptr; cursor = cursor->next) {
-		printf("stormtrooper %c%c-%d\n", 
-				cursor->prefix[0],
-				cursor->prefix[1],
-				cursor->operating_number);
-	}
-
+	raz.announce();
+	jad.announce();
 
 	return 0;
 }
