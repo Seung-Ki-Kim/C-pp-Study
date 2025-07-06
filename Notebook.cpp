3ff2c95a8fdb86a738e071bf3fbc493dd6d95dbb
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2025-07-06 20:52:42 +0900

    값 대입, 복사 파트 공부

--- Notebook.cpp
+++ Notebook.cpp
@@ -1,29 +1,82 @@
+#include <stdexcept>
 #include <cstdio>
+#include <cstring>
 
-void power_up_rat_thing(int nuclear_isotopes) {
-	static thread_local int rat_things_power = 200;	
-	const auto waste_heat = rat_things_power * 20;
+struct SimpleString {
+private:
+    size_t max_size;
+    char* buffer;
+    size_t length;
 
-	rat_things_power = rat_things_power + nuclear_isotopes;
+public:
+    SimpleString(size_t max_size) : max_size { max_size }, length {} {
+        if (max_size == 0) {
+            throw std::runtime_error { "Max size must be at lease 1." };
+        }
 
-	if (waste_heat > 10000) {
-		printf("Warning! Hot doggie!\n");
-	}
+        buffer = new char[max_size];
+        buffer[0] = 0;
+    }
 
-	printf("Rat_thing power: %d\n", rat_things_power);
+    SimpleString& operator = (const SimpleString& other) {
+        if (this == &other) { return *this; }
+
+        const auto new_buffer = new char[other.max_size];
+        delete[] buffer;
+
+        buffer = new_buffer;
+        length = other.length;
+        max_size = other.max_size;
+
+        std::strncpy(buffer, other.buffer,max_size);
+        buffer[max_size - 1] = '\0';
+
+        return *this;
+    }
+
+    ~SimpleString() {
+        delete[] buffer;
+    }
+
+    void print(const char* tag) const {
+        printf("%s: %s", tag, buffer);
+    }
+
+    bool append_line(const char* x) {
+        const auto x_len = strlen(x);   // len of x
+
+        if (x_len + length + 2 > max_size) {
+            return false;
+        }
+
+        std::strncpy(buffer + length, x, max_size - length);
+
+        length += x_len;
+        buffer[length++] = '\n';    // new line
+        buffer[length] = '\0';  // null byte
+
+        return true;
+    }
+};
+
+struct SimpleStringOwner {
+private:
+    SimpleString string;
+
+public:
+    SimpleStringOwner(const SimpleString& my_string) : string{ my_string } {
+    }
+};
+
+void own_a_string() {
+    SimpleString a { 50 };
+    a.append_line("We apologize for the");
+    a.append_line("inconvenience.");
+
+    SimpleString b { a };
 }
 
-
 int main() {
-	power_up_rat_thing(100);
-	power_up_rat_thing(100000);
-
-	int* my_int_ptr = new int{42};
-	printf("%d\n", *my_int_ptr);
-	*my_int_ptr = 30;
-	printf("%d\n", *my_int_ptr);
-	
-	delete my_int_ptr;
 
 
 	return 0;
