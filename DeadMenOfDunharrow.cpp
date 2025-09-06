06c92a5005f99211dd966020bf968fda492be15a
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2025-09-06 15:12:00 +0900

    11장 Unique_ptr 실습 (스마트 포인터)

---
+++ DeadMenOfDunharrow.cpp
@@ -0,0 +1,136 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>   // v3
+#include <memory>
+
+
+struct DeadMenOfDunharrow {
+    DeadMenOfDunharrow(const char* m="") : message{ m } {
+        oaths_to_fulfill++;
+    }
+
+    ~DeadMenOfDunharrow() {
+        oaths_to_fulfill--;
+    }
+
+    const char* message;
+    static int oaths_to_fulfill;
+};
+
+int DeadMenOfDunharrow::oaths_to_fulfill{};
+using ScopedOathbreakers = std::unique_ptr<DeadMenOfDunharrow>;
+
+
+TEST_CASE("UniquePtr evaluates to") {
+    SECTION("True when full") {
+        ScopedOathbreakers aragorn { new DeadMenOfDunharrow{} };
+        REQUIRE(aragorn);
+    }
+
+    SECTION("False when empty") {
+        ScopedOathbreakers aragorn;
+        REQUIRE_FALSE(aragorn);
+    }
+}
+
+TEST_CASE("ScopedPtr is an RAII Wrapper.") {
+    REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 0);
+
+    ScopedOathbreakers aragorn { new DeadMenOfDunharrow{} };
+    REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 1);
+
+    {
+        ScopedOathbreakers legolas { new DeadMenOfDunharrow{} };
+        REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 2);
+    }
+
+    REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 1);
+}
+
+TEST_CASE("ScopedPtr supports pointer semantics, like") {
+    auto message = "The way is shut";
+    ScopedOathbreakers aragorn { new DeadMenOfDunharrow{ message } };
+
+    SECTION("operator*") {
+        REQUIRE((*aragorn).message == message);
+    }
+
+    SECTION("operator->") {
+        REQUIRE(aragorn->message == message);
+    }
+
+    SECTION("get(), which returns a raw pointer") {
+        REQUIRE(aragorn.get() != nullptr);
+    }
+}
+
+TEST_CASE("ScopedPtr supports comparison with nullptr") {
+    SECTION("operator==") {
+        ScopedOathbreakers legolas{};
+        REQUIRE(legolas == nullptr);
+    }
+
+    SECTION("operator!=") {
+        ScopedOathbreakers aragorn{ new DeadMenOfDunharrow{} };
+        REQUIRE(aragorn != nullptr);
+    }
+}
+
+TEST_CASE("ScopedPtr supports swap") {
+    auto message1 = "The way is shut.";
+    auto message2 = "Until the time comes.";
+
+    ScopedOathbreakers aragorn {
+        new DeadMenOfDunharrow{ message1 }
+    };
+
+    ScopedOathbreakers legolas {
+        new DeadMenOfDunharrow{ message2 }
+    };
+
+    aragorn.swap(legolas);
+    REQUIRE(legolas->message == message1);
+    REQUIRE(aragorn->message == message2);
+}
+
+TEST_CASE("ScopedPtr reset") {
+    ScopedOathbreakers aragorn{ new DeadMenOfDunharrow{} };
+
+    SECTION("destructs owned object.") {
+        aragorn.reset();
+        REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 0);
+    }
+
+    SECTION("can replace an owned object.") {
+        auto message = "It was made by those who are Dead.";
+        auto new_dead_man = new DeadMenOfDunharrow{ message };
+
+        REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 2);
+        aragorn.reset(new_dead_man);
+
+        REQUIRE(DeadMenOfDunharrow::oaths_to_fulfill == 1);
+        REQUIRE(aragorn->message == new_dead_man->message);
+        REQUIRE(aragorn.get() == new_dead_man);
+    }
+}
+
+void by_ref(const ScopedOathbreakers&) { }
+void by_val(ScopedOathbreakers) { }
+
+TEST_CASE("ScopedPtr can") {
+    ScopedOathbreakers aragorn{ new DeadMenOfDunharrow };
+
+    SECTION("be passed by reference") {
+        by_ref(aragorn);
+    }
+
+//    SECTION("not be copied") {
+//        by_val(aragorn);
+//        auto son_of_arathorn = aragorn;
+//    }
+
+    SECTION("can be moved") {   // unique_ptr
+        by_val(std::move(aragorn));
+        auto son_of_arathorn = std::move(aragorn);
+    }
+}
